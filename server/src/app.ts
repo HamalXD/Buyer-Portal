e8f8b66def99b3700e1b7c8db507ec8f89c5eeab
hamalxd <sammanhamal06@gmail.com>
2026-03-26 22:38:36 +0545

    fix:port binding while deploying

--- server/src/app.ts
+++ server/src/app.ts
@@ -8,6 +8,8 @@ import { errorHandler } from "./middleware/error.middleware";
 
 dotenv.config();
 
+connectDB();
+
 const app = express();
 
 app.use(cors());
@@ -25,7 +27,5 @@ const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
   console.log(`Server running on ${PORT}`);
 });
-
-connectDB();
 
 app.use(errorHandler);
