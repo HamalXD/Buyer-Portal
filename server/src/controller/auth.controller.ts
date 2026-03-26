c764d8208412274ed8bfb623a30ca8e196f1755e
hamalxd <sammanhamal06@gmail.com>
2026-03-26 21:53:29 +0545

    feat:added user registration and login

---
+++ server/src/controller/auth.controller.ts
@@ -0,0 +1,49 @@
+import { User } from "../models/User";
+import { hashPassword, comparePassword } from "../utils/hash";
+import { generateToken } from "../utils/jwt";
+
+export const register = async (req: any, res: any) => {
+  const { name, email, password } = req.body;
+
+  const exists = await User.findOne({ email });
+
+  if (exists) {
+    return res.status(400).json({ message: "Email already Exists" });
+  }
+
+  const hashed = await hashPassword(password);
+
+  const user = await User.create({
+    name,
+    email,
+    password: hashed,
+  });
+
+  res.json({
+    id: user._id,
+  });
+};
+
+const login = async (req: any, res: any) => {
+  const { email, password } = req.body;
+
+  const user = await User.findOne({ email });
+
+  if (!user) {
+    return res.status(400).json({ messaage: "Invlaid credentials" });
+  }
+
+  const match = await comparePassword(password, user.password);
+
+  if (!match) {
+    return res.status(400).json({ message: "Invlaid credentials" });
+  }
+
+  const token = generateToken(user._id.toString());
+
+  req.json({
+    token,
+    name: user.name,
+    role: user.role,
+  });
+};
