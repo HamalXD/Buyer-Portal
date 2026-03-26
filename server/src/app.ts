e0e7589e469ac81971da9a3bc747ccd73cc702ae
hamalxd <sammanhamal06@gmail.com>
2026-03-26 21:29:03 +0545

    feat: connected mongoDB and updated .env files

--- server/src/app.ts
+++ server/src/app.ts
@@ -1,13 +1,23 @@
 import express from "express";
-require("dotenv").config();
+import cors from "cors";
+import dotenv from "dotenv";
+import { connectDB } from "./config/db";
+
+dotenv.config();
 
 const app = express();
-const port = 3000;
 
-app.get("/", (req, res) => {
-  res.send("Hello World from Express & TypeScript!");
+app.use(cors());
+app.use(express.json());
+
+app.get("/", (_req, res) => {
+  res.send("API running");
 });
 
-app.listen(port, () => {
-  console.log(`Server is running at ${process.env.PORT}`);
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, () => {
+  console.log(`Server running on ${PORT}`);
 });
+
+connectDB();
