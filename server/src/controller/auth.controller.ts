6fffff22ffb27c8a6fc8442e23e5949fc2bc31bf
hamalxd <sammanhamal06@gmail.com>
2026-03-26 22:24:47 +0545

    feat:added try catch in controllers for better error handling

--- server/src/controller/auth.controller.ts
+++ server/src/controller/auth.controller.ts
@@ -2,48 +2,56 @@ import { User } from "../models/User";
 import { hashPassword, comparePassword } from "../utils/hash";
 import { generateToken } from "../utils/jwt";
 
-export const register = async (req: any, res: any) => {
-  const { name, email, password } = req.body;
+export const register = async (req: any, res: any, next: any) => {
+  try {
+    const { name, email, password } = req.body;
 
-  const exists = await User.findOne({ email });
+    const exists = await User.findOne({ email });
 
-  if (exists) {
-    return res.status(400).json({ message: "Email already Exists" });
+    if (exists) {
+      return res.status(400).json({ message: "Email already Exists" });
+    }
+
+    const hashed = await hashPassword(password);
+
+    const user = await User.create({
+      name,
+      email,
+      password: hashed,
+    });
+
+    res.json({
+      id: user._id,
+    });
+  } catch (error) {
+    next(error);
   }
-
-  const hashed = await hashPassword(password);
-
-  const user = await User.create({
-    name,
-    email,
-    password: hashed,
-  });
-
-  res.json({
-    id: user._id,
-  });
 };
 
-export const login = async (req: any, res: any) => {
-  const { email, password } = req.body;
+export const login = async (req: any, res: any, next: any) => {
+  try {
+    const { email, password } = req.body;
 
-  const user = await User.findOne({ email });
+    const user = await User.findOne({ email });
 
-  if (!user) {
-    return res.status(400).json({ messaage: "Invlaid credentials" });
+    if (!user) {
+      return res.status(400).json({ messaage: "Invlaid credentials" });
+    }
+
+    const match = await comparePassword(password, user.password);
+
+    if (!match) {
+      return res.status(400).json({ message: "Invlaid credentials" });
+    }
+
+    const token = generateToken(user._id.toString());
+
+    res.json({
+      token,
+      name: user.name,
+      role: user.role,
+    });
+  } catch (error) {
+    next(error);
   }
-
-  const match = await comparePassword(password, user.password);
-
-  if (!match) {
-    return res.status(400).json({ message: "Invlaid credentials" });
-  }
-
-  const token = generateToken(user._id.toString());
-
-  res.json({
-    token,
-    name: user.name,
-    role: user.role,
-  });
 };
--- server/src/controller/favourites.controller.ts
+++ server/src/controller/favourites.controller.ts
@@ -1,40 +1,52 @@
 import { Favourite } from "../models/Favourite";
 
-export const getFavourites = async (req: any, res: any) => {
-  const favourites = await Favourite.find({
-    user: req.userId,
-  }).populate("property");
+export const getFavourites = async (req: any, res: any, next: any) => {
+  try {
+    const favourites = await Favourite.find({
+      user: req.userId,
+    }).populate("property");
 
-  res.json(favourites);
-};
-
-export const addFavourite = async (req: any, res: any) => {
-  const propertyId = req.param.id;
-
-  const exists = await Favourite.findOne({
-    user: req.userId,
-    property: propertyId,
-  });
-
-  if (!exists) {
-    return res.status(400).json({ message: "Already favourited" });
+    res.json(favourites);
+  } catch (error) {
+    next(error);
   }
-
-  await Favourite.create({
-    user: req.userId,
-    property: propertyId,
-  });
-
-  res.json({ message: "Added to favourites" });
 };
 
-export const removeFavourite = async (req: any, res: any) => {
-  const propertyId = req.params.id;
+export const addFavourite = async (req: any, res: any, next: any) => {
+  try {
+    const propertyId = req.param.id;
 
-  await Favourite.findOneAndDelete({
-    user: req.userId,
-    property: propertyId,
-  });
+    const exists = await Favourite.findOne({
+      user: req.userId,
+      property: propertyId,
+    });
 
-  res.json({ message: "Removed from favourites" });
+    if (!exists) {
+      return res.status(400).json({ message: "Already favourited" });
+    }
+
+    await Favourite.create({
+      user: req.userId,
+      property: propertyId,
+    });
+
+    res.json({ message: "Added to favourites" });
+  } catch (error) {
+    next(error);
+  }
+};
+
+export const removeFavourite = async (req: any, res: any, next: any) => {
+  try {
+    const propertyId = req.params.id;
+
+    await Favourite.findOneAndDelete({
+      user: req.userId,
+      property: propertyId,
+    });
+
+    res.json({ message: "Removed from favourites" });
+  } catch (error) {
+    next(error);
+  }
 };
